f78e0cb6d283944a85869ed3c79af2ce676a76aa
hk.tgmrecords <hk.tgmrecords@gmail.com>
2013-09-24 13:45:04 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3b740c9d1c13c1071a7d3be96bd7d922e9a68d116b19d256de541b9d121ca4d461901facd18ddc67ec0b8af6a1e2fef45c081b6865cca0f45b0a0fce782e5004'
